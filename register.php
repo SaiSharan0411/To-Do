f96f855b322f71982cf899a678a8777a946e5183
sai-sharan <saisharan0411@gmail.com>
2023-03-02 20:48:39 +0530

    1

--- register.php
+++ register.php
@@ -9,8 +9,7 @@ $password = mysqli_real_escape_string($con, $_POST['password']);
 $check_query = "SELECT * FROM user WHERE uname = '$uname'";
 $check_result = mysqli_query($con, $check_query);
 if(mysqli_num_rows($check_result) > 0) {
-echo '<script type="text/javascript">
-alert("Username already exists");
+echo '<script type="text/javascript">alert("Username already exists");
 window.location = "register.html";
 </script>';
 exit();
