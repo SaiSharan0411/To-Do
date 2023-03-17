200446afc0a49a0d1b646c0141f342f5c3e2af4e
sai-sharan <saisharan0411@gmail.com>
2023-03-17 20:10:37 +0530

    1

---
+++ status.php
@@ -0,0 +1,65 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="page.css">
+</head>
+<body><center><br>
+<h2>In-Complete Tasks</h2>
+<?php
+session_start();
+$con = mysqli_connect("localhost","root","","todo");
+$uname = $_SESSION['uname'];
+if (isset($_POST['completed'])) {
+$tname = $_POST['tname'];
+$query = "UPDATE list SET status='completed' WHERE uname='$uname' AND tname='$tname'";
+mysqli_query($con, $query);
+}
+$query = "SELECT tname, date, time, status FROM list WHERE uname = '$uname' AND status = 'incomplete' ORDER BY date, time";
+$result = mysqli_query($con, $query);
+echo '<table>';
+echo '<tr><th>Task</th><th>Date</th><th>Time</th><th>Status</th><th>Action</th></tr>';
+while ($task = mysqli_fetch_assoc($result)) {
+echo '<tr>';
+echo '<td>' . $task['tname'] . '</td>';
+echo '<td>' . $task['date'] . '</td>';
+echo '<td>' . $task['time'] . '</td>';
+echo '<td>' . $task['status'] . '</td>';
+echo '<td>
+<form action="" method="post">
+<input type="hidden" name="tname" value="' . $task['tname'] . '">
+<input type="submit" name="completed" value="Complete">
+</form>
+</td>';
+echo '</tr>';
+}
+echo '<a href="tasks.php"><input type="button" value="Home"></a><br><br>';
+echo '</table>';
+?>
+<br>
+<h2>Completed Tasks</h2>
+<?php
+if (isset($_POST['incompleted'])) {
+$query = "UPDATE list SET status='incomplete' WHERE uname='$uname' AND tname='$tname'";
+mysqli_query($con, $query);
+}
+$query = "SELECT tname, date, time, status FROM list WHERE uname = '$uname' AND status = 'completed' ORDER BY date, time";
+$result = mysqli_query($con, $query);
+echo '<table>';
+echo '<tr><th><br>Task</th><th><br>Date</th><th><br>Time</th><th><br>Status</th><th><br>Action</th></tr>';
+while ($task = mysqli_fetch_assoc($result)) {
+echo '<tr>';
+echo '<td>' . $task['tname'] . '</td>';
+echo '<td>' . $task['date'] . '</td>';
+echo '<td>' . $task['time'] . '</td>';
+echo '<td>' . $task['status'] . '</td>';
+echo '<td>
+<form action="" method="post">
+<input type="hidden" name="tname" value="' . $task['tname'] . '">
+<input type="submit" name="incompleted" value="Incomplete">
+</form>
+</td>';
+echo '</tr>';
+}
+echo '</table>';
+?></center>
+</body>
+</html>
--- tasks.php
+++ tasks.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>Tasks</title>
-<link rel="stylesheet" type="text/css" href="taskspage.css">
+<link rel="stylesheet" type="text/css" href="tasks.css">
 </head>
 <body><br><center>
 <?php
@@ -13,23 +13,16 @@ $uname = $_SESSION['uname'];
 <?php
 $query = "SELECT tname, date, time, status FROM list WHERE uname = '$uname' ORDER BY date, time";
 $result = mysqli_query($con, $query);
-echo '<br><form method="post">';
-echo '<br><div class="form-row"><div><input type="text" name="tname" placeholder="Task Name">';
-echo '<input type="date" name="date">';
-echo '<input type="time" name="time">';
-echo '<input type="submit" value="Add Task"></div></div><br>';
-echo '<div class="button-row"><a href="incomplete.php"><input type="button" value="In-Complete"></a>';
-echo '<a href="today.php"><input type="button" value="Day"></a>';
-echo '<a href="month.php"><input type="button" value="Month"></a>';
-echo '<a href="complete.php"><input type="button" value="Completed"></a></div><br><br><br>';
+echo '<br><form method="post"><br>';
+echo '<div class="button-row"><a href="addtask.php"><input type="button" value="Add Task"></a>';
+echo '<a href="status.php"><input type="button" value="Status"></a>';
+echo '<a href="login.html"><input type="button" value="Log-Out"></a></div><br>';
 echo '</form>';
-if (!empty($_POST['tname'])) {
-$tname = $_POST['tname'];
-$date = $_POST['date'];
-$time = $_POST['time'];
-$query = "INSERT INTO list (uname, tname, date, time) VALUES ('$uname', '$tname', '$date', '$time')";
-mysqli_query($con, $query);
-}
+echo '<p>Our project main page is where you can manage all your tasks easily. 
+You can add new tasks, edit existing ones, or delete them if they are no longer needed. 
+You can also view your tasks in different filters, such as by date, week, month, year or status. 
+This will help you keep track of your progress and stay organized. 
+My project main page is user-friendly and intuitive, so you can get started right away!</p>';
 ?>
 </center></body>
 </html>
