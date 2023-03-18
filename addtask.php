69df59becadf8ca0a619fcae1184e67a22e1f868
sai-sharan <saisharan0411@gmail.com>
2023-03-18 23:11:58 +0530

    1

--- addtask.php
+++ addtask.php
@@ -1,6 +1,5 @@
 <html>
 <head>
-<title>Incomplete Tasks</title>
 <link rel="stylesheet" type="text/css" href="addtask.css">
 </head>
 <body><br><center>
@@ -12,11 +11,11 @@ $uname = $_SESSION['uname'];
 <h1>Add Tasks to your To-Do List <?php echo $uname; ?></h1>
 <?php
 echo '<a href="tasks.php"><input type="button" value="Home"></a><br>';
-echo '<br><form method="post"><br>';
+echo '<br><form method="post">';
 echo '<br><div class="form-row"><div><input type="text" name="tname" placeholder="Task Name" required>';
 echo '<input type="date" name="date" required>';
 echo '<input type="time" name="time" required>';
-echo '<input type="submit" value="Add Task"></div></div><br><br>';
+echo '<input type="submit" value="Add Task"></div></div><br>';
 echo '</form>';
 if (!empty($_POST['tname'])) {
 $tname = $_POST['tname'];
